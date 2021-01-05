29f6dce0dde1510f65d118c1671c2ec4bfda060d
lctam <tam.luchan@gmail.com>
2021-01-05 14:16:34 +0700

    h4

--- TestJenk/Program.cs
+++ TestJenk/Program.cs
@@ -10,6 +10,7 @@ namespace TestJenk
             Console.WriteLine("Hello World! 2");
             Console.WriteLine("Hello World! 3");
             Console.WriteLine("Hello World! 4");
+            Console.WriteLine("Hello World! 5");
         }
     }
 }
